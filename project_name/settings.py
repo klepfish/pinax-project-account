a81afddd5d505e1556953b52f2af10aed5654274
Leon Klepfish <klepfish@Leons-MacBook-Air.local>
2012-09-12 19:11:46 -0700

    changed location of static files

--- project_name/settings.py
+++ project_name/settings.py
@@ -63,15 +63,15 @@ MEDIA_URL = "/site_media/media/"
 # Don"t put anything in this directory yourself; store your static files
 # in apps" "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "static")
+STATIC_ROOT = os.path.join(PACKAGE_ROOT, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/site_media/static/"
+STATIC_URL = "/static/"
 
 # Additional locations of static files
 STATICFILES_DIRS = [
-    os.path.join(PACKAGE_ROOT, "static"),
+#    os.path.join(PACKAGE_ROOT, "static"),
 ]
 
 # List of finder classes that know how to find static files in
